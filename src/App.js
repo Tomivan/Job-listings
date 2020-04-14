a7424e075497e379fa5da0bf3a75acafb218ee48
Tomivan <gracejunkie20@gmail.com>
2020-04-14 15:57:33 +0100

    project initial structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import ReactDOM from 'react-dom';
+import data from './data.json';
+import JobBoard from './components/JobBoard';
 import './App.css';
 
 function App() {
+  const [jobs, setJobs] = useState([]);
+
+    useEffect(() => setJobs(data), []);
+
   return (
     <div className="App">
-      
+      {
+        jobs.length === 0 ? (
+          <p>Jobs are fetching...</p>
+        ) : (
+          jobs.map(job =>(
+            <JobBoard job={job} key={job.id} />
+          ))
+        )
+      }
     </div>
   );
 }
 
+ReactDOM.render(<App />,
+  document.querySelector('#root'));
 export default App;
---
+++ src/components/JobBoard.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import '../App.css';
+
+const JobBoard = (props) => (
+
+        <div className="card">
+            <div className="image">
+                <img src={props.job.logo} alt={props.job.company}/>
+                <p>{props.job.company}</p>
+            </div>
+            <div className="role">
+                <h3>
+                    {props.job.company}
+                    {props.job.new && (
+                <span>New</span> 
+                    )}
+                    {props.job.featured && (
+                <span>Featured</span>
+                    )}
+                </h3>
+                <h5>{props.job.position}</h5>
+            </div> 
+            <div className="details">
+                <p>{props.job.postedAt}</p>
+                <p>{props.job.contract}</p>
+                <p>{props.job.location}</p>
+            </div>
+            <hr/>
+            <p>{props.job.languages && props.job.tools}</p>
+        </div>
+);
+export default JobBoard
